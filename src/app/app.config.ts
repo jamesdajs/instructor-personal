cfcf6054865adc0f2cdfa746ed45d7b3db1ddc4f
Daniel Jaimes <james.rock.alt@gmail.com>
2019-05-06 11:52:35 -0400

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,27 +3,27 @@ import { Timestamp } from "rxjs/internal/operators/timestamp";
 
 export const firebaseConfig = {
   production: false,
-    firebase:{
-      apiKey: "AIzaSyCM_hCpwHHNA82t0GQhnMhAAfmEdsXORRk",
-      authDomain: "good-me-pruevas.firebaseapp.com",
-      databaseURL: "https://good-me-pruevas.firebaseio.com",
-      projectId: "good-me-pruevas",
-      storageBucket: "good-me-pruevas.appspot.com",
-      messagingSenderId: "677999497931"
-    },
-    cliente_endpoint: "cliente",
-    instructor_endpoint: "instructor"
-  };
-   
-  
-/*  configuracion usuarios 
+  firebase: {
+    apiKey: "AIzaSyDv9zk84ed6_ckRorvxhVnTfecLs_aSbFw",
+    authDomain: "entrenador-personal-20e1f.firebaseapp.com",
+    databaseURL: "https://entrenador-personal-20e1f.firebaseio.com",
+    projectId: "entrenador-personal-20e1f",
+    storageBucket: "entrenador-personal-20e1f.appspot.com",
+    messagingSenderId: "150980917345"
+  },
+  cliente_endpoint: "cliente",
+  instructor_endpoint: "instructor"
+};
+
+
+/*  configuracion pruevas 
     apiKey: "AIzaSyCM_hCpwHHNA82t0GQhnMhAAfmEdsXORRk",
     authDomain: "good-me-pruevas.firebaseapp.com",
     databaseURL: "https://good-me-pruevas.firebaseio.com",
     projectId: "good-me-pruevas",
     storageBucket: "good-me-pruevas.appspot.com",
     messagingSenderId: "677999497931"
-////////// Configuracion para pruevas
+////////// Configuracion usuarios
       apiKey: "AIzaSyDv9zk84ed6_ckRorvxhVnTfecLs_aSbFw",
       authDomain: "entrenador-personal-20e1f.firebaseapp.com",
       databaseURL: "https://entrenador-personal-20e1f.firebaseio.com",
@@ -33,22 +33,22 @@ export const firebaseConfig = {
 
 */
 
-  export interface Cliente {
-    email: string;
-    nombre: string;
-    foto:string;
-    peso:number;
-    altura:number;
-    telefono:number;
-    genero:boolean;
-    fechanac:Timestamp<any>;
-    instructor:boolean,
-    descorta:string
-  }
-  
-  export interface Instructor {
-    message: string;
-    pair: string;
-    sender: string;
-    time: number;
-  }
+export interface Cliente {
+  email: string;
+  nombre: string;
+  foto: string;
+  peso: number;
+  altura: number;
+  telefono: number;
+  genero: boolean;
+  fechanac: Timestamp<any>;
+  instructor: boolean,
+  descorta: string
+}
+
+export interface Instructor {
+  message: string;
+  pair: string;
+  sender: string;
+  time: number;
+}
